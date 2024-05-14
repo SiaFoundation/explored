ef466ab79e03ae45bde24787fba812098adea06a
Christopher Tarry <chrisjtarry@gmail.com>
2024-05-13 20:32:28 -0400

    add storage utilization

--- persist/sqlite/metrics.go
+++ persist/sqlite/metrics.go
@@ -10,7 +10,14 @@ import (
 func (s *Store) Metrics() (result explorer.Metrics, err error) {
 	err = s.transaction(func(tx *txn) error {
 		totalHosts := func() (count uint64, err error) {
-			err = tx.QueryRow("SELECT COUNT(*) FROM (SELECT DISTINCT public_key FROM host_announcements);").Scan(&count)
+			err = tx.QueryRow(`SELECT COUNT(*) FROM (SELECT DISTINCT public_key FROM host_announcements);`).Scan(&count)
+			return
+		}
+		storageUtilization := func() (count uint64, err error) {
+			err = tx.QueryRow(`SELECT SUM(fce.filesize)
+FROM file_contract_elements fce
+LEFT JOIN last_contract_revision rev ON (rev.contract_element_id = fce.id)
+WHERE fce.valid = TRUE AND fce.resolved = FALSE`).Scan(&count)
 			return
 		}
 
@@ -22,9 +29,15 @@ func (s *Store) Metrics() (result explorer.Metrics, err error) {
 
 		hosts, err := totalHosts()
 		if err != nil {
-			return fmt.Errorf("failed to get tip: %w", err)
+			return fmt.Errorf("failed to get total hosts: %w", err)
 		}
 		result.TotalHosts = hosts
+
+		bytes, err := storageUtilization()
+		if err != nil {
+			return fmt.Errorf("failed to get storage utilization: %w", err)
+		}
+		result.StorageUtilization = bytes
 		return nil
 	})
 	return
