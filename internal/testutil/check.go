9322785c540ffa69451c01d463990281f65d2040
Christopher Tarry <chrisjtarry@gmail.com>
2025-05-23 13:57:10 -0400

    remove old test helpers

--- internal/testutil/check.go
+++ internal/testutil/check.go
@@ -28,19 +28,6 @@ func Equal[T any](t *testing.T, desc string, expected, got T) {
 	}
 }
 
-// CheckBalance checks that an address has the balances we expect.
-func CheckBalance(t *testing.T, db explorer.Store, addr types.Address, expectSC, expectImmatureSC types.Currency, expectSF uint64) {
-	t.Helper()
-
-	sc, immatureSC, sf, err := db.Balance(addr)
-	if err != nil {
-		t.Fatal(err)
-	}
-	Equal(t, "siacoins", expectSC, sc)
-	Equal(t, "immature siacoins", expectImmatureSC, immatureSC)
-	Equal(t, "siafunds", expectSF, sf)
-}
-
 // CheckTransaction checks the inputs and outputs of the retrieved transaction
 // with the source transaction.
 func CheckTransaction(t *testing.T, expectTxn types.Transaction, gotTxn explorer.Transaction) {
@@ -312,52 +299,6 @@ func CheckV2Transaction(t *testing.T, expectTxn types.V2Transaction, gotTxn expl
 	Equal(t, "arbitrary data", len(expectTxn.ArbitraryData), len(gotTxn.ArbitraryData))
 	for i := range expectTxn.ArbitraryData {
 		Equal(t, "arbitrary data value", expectTxn.ArbitraryData[i], gotTxn.ArbitraryData[i])
-	}
-}
-
-// CheckV2ChainIndices checks that the chain indices that a v2 transaction was
-// in from the explorer match the expected chain indices.
-func CheckV2ChainIndices(t *testing.T, db explorer.Store, txnID types.TransactionID, expected []types.ChainIndex) {
-	t.Helper()
-
-	indices, err := db.V2TransactionChainIndices(txnID, 0, 100)
-	switch {
-	case err != nil:
-		t.Fatal(err)
-	case len(indices) != len(expected):
-		t.Fatalf("expected %d indices, got %d", len(expected), len(indices))
-	}
-	for i := range indices {
-		Equal(t, "index", expected[i], indices[i])
-	}
-}
-
-// CheckFC checks the retrieved file contract with the source file contract in
-// addition to checking the resolved and valid fields.
-func CheckFC(t *testing.T, revision, resolved, valid bool, expected types.FileContract, got explorer.ExtendedFileContract) {
-	t.Helper()
-
-	Equal(t, "resolved state", resolved, got.Resolved)
-	Equal(t, "valid state", valid, got.Valid)
-
-	Equal(t, "filesize", expected.Filesize, got.Filesize)
-	Equal(t, "file merkle root", expected.FileMerkleRoot, got.FileMerkleRoot)
-	Equal(t, "window start", expected.WindowStart, got.WindowStart)
-	Equal(t, "window end", expected.WindowEnd, got.WindowEnd)
-	if !revision {
-		Equal(t, "payout", expected.Payout, got.Payout)
-	}
-	Equal(t, "unlock hash", expected.UnlockHash, got.UnlockHash)
-	Equal(t, "revision number", expected.RevisionNumber, got.RevisionNumber)
-	Equal(t, "valid proof outputs", len(expected.ValidProofOutputs), len(got.ValidProofOutputs))
-	for i := range expected.ValidProofOutputs {
-		Equal(t, "valid proof output address", expected.ValidProofOutputs[i].Address, got.ValidProofOutputs[i].Address)
-		Equal(t, "valid proof output value", expected.ValidProofOutputs[i].Value, got.ValidProofOutputs[i].Value)
-	}
-	Equal(t, "missed proof outputs", len(expected.MissedProofOutputs), len(got.MissedProofOutputs))
-	for i := range expected.MissedProofOutputs {
-		Equal(t, "missed proof output address", expected.MissedProofOutputs[i].Address, got.MissedProofOutputs[i].Address)
-		Equal(t, "missed proof output value", expected.MissedProofOutputs[i].Value, got.MissedProofOutputs[i].Value)
 	}
 }
 
