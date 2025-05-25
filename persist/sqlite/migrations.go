4ab3eac85217ffaab9450b1b3b3fd70769e03739
Nate <me@n8m.us>
2025-05-25 10:27:06 -0700

    sqlite: add missing foreign keys

--- persist/sqlite/migrations.go
+++ persist/sqlite/migrations.go
@@ -2,4 +2,19 @@ package sqlite
 
 import "go.uber.org/zap"
 
-var migrations []func(tx *txn, log *zap.Logger) error
+func migrateV2(txn *txn, _ *zap.Logger) error {
+	const createForeignKeyIndices = `
+CREATE INDEX siacoin_elements_blocks_id_index ON siacoin_elements(block_id);
+CREATE INDEX siafund_elements_blocks_id_index ON siafund_elements(block_id);
+CREATE INDEX file_contract_elements_blocks_id_index ON file_contract_elements(block_id);
+CREATE INDEX last_contract_revision_confirmation_block_id_index ON last_contract_revision(confirmation_block_id);
+CREATE INDEX v2_file_contract_elements_blocks_id_index ON v2_file_contract_elements(block_id);
+CREATE INDEX v2_last_contract_revision_confirmation_block_id_index ON v2_last_contract_revision(confirmation_block_id);
+`
+	_, err := txn.Exec(createForeignKeyIndices)
+	return err
+}
+
+var migrations = []func(tx *txn, log *zap.Logger) error{
+	migrateV2,
+}
