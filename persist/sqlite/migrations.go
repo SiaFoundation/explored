aa65396da48cc15c3be54747ccf68675469620b2
Christopher Tarry <chrisjtarry@gmail.com>
2025-06-03 16:57:37 -0400

    add index on certain foreign keys to make revert faster

--- persist/sqlite/migrations.go
+++ persist/sqlite/migrations.go
@@ -15,6 +15,20 @@ CREATE INDEX v2_last_contract_revision_confirmation_block_id_index ON v2_last_co
 	return err
 }
 
+func migrateV3(txn *txn, _ *zap.Logger) error {
+	const createForeignKeyIndices = `
+CREATE INDEX file_contract_elements_transactions_id_index ON file_contract_elements(transaction_id);
+CREATE INDEX last_contract_revision_contract_element_id_index ON last_contract_revision(contract_element_id);
+CREATE INDEX v1_transaction_events_transaction_id_index ON v1_transaction_events(transaction_id);
+CREATE INDEX v2_transaction_events_transaction_id_index ON v2_transaction_events(transaction_id);
+CREATE INDEX v2_file_contract_elements_transaction_id_index ON v2_file_contract_elements(transaction_id);
+CREATE INDEX v2_last_contract_revision_contract_element_id_index ON v2_last_contract_revision(contract_element_id);
+`
+	_, err := txn.Exec(createForeignKeyIndices)
+	return err
+}
+
 var migrations = []func(tx *txn, log *zap.Logger) error{
 	migrateV2,
+	migrateV3,
 }
